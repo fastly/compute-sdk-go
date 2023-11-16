2360bfee7994f7f1b5a8c628dcaf7f997c6277bc
Joe Shaw <jshaw@fastly.com>
2023-11-16 12:53:01 -0500

    simplify some integration tests that don't need to be run in a request

--- integration_tests/secret_store/main_test.go
+++ integration_tests/secret_store/main_test.go
@@ -3,97 +3,46 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"testing"
 
-	"github.com/fastly/compute-sdk-go/fsthttp"
-	"github.com/fastly/compute-sdk-go/fsttest"
 	"github.com/fastly/compute-sdk-go/secretstore"
 )
 
 func TestSecretStore(t *testing.T) {
-	handler := func(ctx context.Context, w fsthttp.ResponseWriter, r *fsthttp.Request) {
-		st, err := secretstore.Open("phrases")
-		switch {
-		case errors.Is(err, secretstore.ErrSecretStoreNotFound):
-			fsthttp.Error(w, err.Error(), fsthttp.StatusNotFound)
-			return
-		case err != nil:
-			fsthttp.Error(w, err.Error(), fsthttp.StatusInternalServerError)
-			return
-		}
-
-		s, err := st.Get("my_phrase")
-		switch {
-		case errors.Is(err, secretstore.ErrSecretNotFound):
-			fsthttp.Error(w, err.Error(), fsthttp.StatusNotFound)
-			return
-		case err != nil:
-			fsthttp.Error(w, err.Error(), fsthttp.StatusInternalServerError)
-			return
-		}
-
-		v, err := s.Plaintext()
-		if err != nil {
-			fsthttp.Error(w, err.Error(), fsthttp.StatusInternalServerError)
-			return
-		}
-
-		w.Write(v)
-	}
-
-	r, err := fsthttp.NewRequest("GET", "/", nil)
+	st, err := secretstore.Open("phrases")
 	if err != nil {
-		t.Fatalf("NewRequest: %v", err)
+		t.Fatal(err)
 	}
 
-	w := fsttest.NewRecorder()
-
-	handler(context.Background(), w, r)
-
-	if got, want := w.Code, fsthttp.StatusOK; got != want {
-		t.Errorf("Code = %d, want %d", got, want)
+	s, err := st.Get("my_phrase")
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	if got, want := w.Body.String(), "sssh! don't tell anyone!"; got != want {
-		t.Errorf("Body = %q, want %q", got, want)
+	v, err := s.Plaintext()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := string(v), "sssh! don't tell anyone!"; got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
 
 func TestSecretFromBytes(t *testing.T) {
 	const plaintext = "not a real secret"
 
-	handler := func(ctx context.Context, w fsthttp.ResponseWriter, r *fsthttp.Request) {
-		s, err := secretstore.SecretFromBytes([]byte(plaintext))
-		if err != nil {
-			fsthttp.Error(w, err.Error(), fsthttp.StatusInternalServerError)
-			return
-		}
-
-		v, err := s.Plaintext()
-		if err != nil {
-			fsthttp.Error(w, err.Error(), fsthttp.StatusInternalServerError)
-			return
-		}
-
-		w.Write(v)
-	}
-
-	r, err := fsthttp.NewRequest("GET", "/", nil)
+	s, err := secretstore.SecretFromBytes([]byte(plaintext))
 	if err != nil {
-		t.Fatalf("NewRequest: %v", err)
+		t.Fatal(err)
 	}
 
-	w := fsttest.NewRecorder()
-
-	handler(context.Background(), w, r)
-
-	if got, want := w.Code, fsthttp.StatusOK; got != want {
-		t.Errorf("Code = %d, want %d", got, want)
+	v, err := s.Plaintext()
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	if got, want := w.Body.String(), plaintext; got != want {
-		t.Errorf("Body = %q, want %q", got, want)
+	if got, want := string(v), plaintext; got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
