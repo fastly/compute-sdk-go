f04021b8eaf93cc98147d1dce3035c31211b290d
Adam Wick <awick@fastly.com>
2024-08-16 14:58:21 -0700

    add a test to make sure that vcpu time advances

--- integration_tests/compute_runtime/main_test.go
+++ integration_tests/compute_runtime/main_test.go
@@ -24,4 +24,27 @@ func TestGetVcpuMs(t *testing.T) {
   if end - start > time.Second {
     t.Errorf("Sleeping shouldn't count as vcpu time!")
   }
+
+  now, err := compute_runtime.GetVCPUTime()
+  if err != nil {
+    t.Errorf("Couldn't get starting vcpu time")
+  }
+
+  var counter uint64
+  var next time.Duration
+
+  counter = 0
+  next = now
+  for now == next {
+    new_next, err := compute_runtime.GetVCPUTime()
+    if err != nil {
+      t.Errorf("Couldn't get starting vcpu time")
+    }
+    next = new_next
+    counter += 1
+  }
+
+  if counter == 0 {
+    t.Errorf("It should take at least one loop to advance vcpu time")
+  }
 }
