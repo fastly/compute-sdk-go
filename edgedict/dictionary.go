83821d0948613229995195cbf1f86928064b53aa
Damian Gryski <damian@gryski.com>
2024-08-28 11:18:18 -0700

    edgedict: import GetBytes and Has from configstore

--- edgedict/dictionary.go
+++ edgedict/dictionary.go
@@ -4,6 +4,7 @@ package edgedict
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/fastly/compute-sdk-go/internal/abi/fastly"
 )
@@ -26,6 +27,9 @@ var (
 
 	// ErrKeyNotFound indicates a key isn't in a dictionary.
 	ErrKeyNotFound = errors.New("key not found")
+
+	// ErrUnexpected indicates an unexpected error occurred.
+	ErrUnexpected = errors.New("unexpected error")
 )
 
 // Dictionary is a read-only representation of an edge dictionary.
@@ -59,24 +63,59 @@ func Open(name string) (*Dictionary, error) {
 	return &Dictionary{d}, nil
 }
 
-// Get returns the item in the dictionary with the given key.
-func (d *Dictionary) Get(key string) (string, error) {
+// GetBytes returns the value in the dictionary for the given key, if it exists, as a byte slice.
+func (d *Dictionary) GetBytes(key string) ([]byte, error) {
 	if d == nil {
-		return "", ErrKeyNotFound
+		return nil, ErrKeyNotFound
 	}
 
-	s, err := d.abiDict.Get(key)
+	v, err := d.abiDict.GetBytes(key)
 	if err != nil {
 		status, ok := fastly.IsFastlyError(err)
 		switch {
 		case ok && status == fastly.FastlyStatusBadf:
-			return "", ErrDictionaryNotFound
+			return nil, ErrDictionaryNotFound
 		case ok && status == fastly.FastlyStatusNone:
-			return "", ErrKeyNotFound
+			return nil, ErrKeyNotFound
+		case ok:
+			return nil, fmt.Errorf("%w (%s)", ErrUnexpected, status)
 		default:
-			return "", err
+			return nil, err
+		}
+	}
+	return v, nil
+}
+
+// Get returns the value in the dictionary with the given key, if it exists.
+func (d *Dictionary) Get(key string) (string, error) {
+	buf, err := d.GetBytes(key)
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
+
+// Has returns true if the key exists in the dictionary, without allocating
+// space to read a value.
+func (d *Dictionary) Has(key string) (bool, error) {
+	if d == nil {
+		return false, ErrKeyNotFound
+	}
+
+	v, err := d.abiDict.Has(key)
+	if err != nil {
+		status, ok := fastly.IsFastlyError(err)
+		switch {
+		case ok && status == fastly.FastlyStatusBadf:
+			return false, ErrDictionaryNotFound
+		case ok && status == fastly.FastlyStatusNone:
+			return false, ErrKeyNotFound
+		case ok:
+			return false, fmt.Errorf("%w (%s)", ErrUnexpected, status)
+		default:
+			return false, err
 		}
 	}
 
-	return s, nil
+	return v, nil
 }
