d05a69e7332eca8ed7ae978fc4903b69532740d0
Damian Gryski <damian@gryski.com>
2024-08-28 11:10:31 -0700

    edgedict: restore edgedict implementation
    The configstore package is moving to the new configstore hostcalls, but we
    don't want to surprise users who are still using edgedict, so ensure users keep
    using the old hostcalls by splitting the implementations.

--- edgedict/dictionary.go
+++ edgedict/dictionary.go
@@ -3,38 +3,80 @@
 package edgedict
 
 import (
-	"github.com/fastly/compute-sdk-go/configstore"
+	"errors"
+
+	"github.com/fastly/compute-sdk-go/internal/abi/fastly"
 )
 
 var (
 	// ErrDictionaryNotFound indicates the named dictionary doesn't exist.
-	ErrDictionaryNotFound = configstore.ErrStoreNotFound
+	ErrDictionaryNotFound = errors.New("dictionary not found")
 
 	// ErrDictionaryNameEmpty indicates the given dictionary name
 	// was empty.
-	ErrDictionaryNameEmpty = configstore.ErrStoreNameEmpty
+	ErrDictionaryNameEmpty = errors.New("dictionary name was empty")
 
 	// ErrDictionaryNameInvalid indicates the given dictionary name
 	// was invalid.
-	ErrDictionaryNameInvalid = configstore.ErrStoreNameInvalid
+	ErrDictionaryNameInvalid = errors.New("dictionary name contained invalid characters")
 
 	// ErrDictionaryNameTooLong indicates the given dictionary name
 	// was too long.
-	ErrDictionaryNameTooLong = configstore.ErrStoreNameTooLong
+	ErrDictionaryNameTooLong = errors.New("dictionary name too long")
 
 	// ErrKeyNotFound indicates a key isn't in a dictionary.
-	ErrKeyNotFound = configstore.ErrKeyNotFound
+	ErrKeyNotFound = errors.New("key not found")
 )
 
 // Dictionary is a read-only representation of an edge dictionary.
 //
 // Deprecated: Use the configstore package instead.
-type Dictionary = configstore.Store
+type Dictionary struct {
+	abiDict *fastly.Dictionary
+}
 
 // Open returns an edge dictionary with the given name. Names are case
 // sensitive.
 //
 // Deprecated: Use configstore.Open() instead.
 func Open(name string) (*Dictionary, error) {
-	return configstore.Open(name)
+	d, err := fastly.OpenDictionary(name)
+	if err != nil {
+		status, ok := fastly.IsFastlyError(err)
+		switch {
+		case ok && status == fastly.FastlyStatusBadf:
+			return nil, ErrDictionaryNotFound
+		case ok && status == fastly.FastlyStatusNone:
+			return nil, ErrDictionaryNameEmpty
+		case ok && status == fastly.FastlyStatusUnsupported:
+			return nil, ErrDictionaryNameTooLong
+		case ok && status == fastly.FastlyStatusInval:
+			return nil, ErrDictionaryNameInvalid
+		default:
+			return nil, err
+		}
+	}
+	return &Dictionary{d}, nil
+}
+
+// Get returns the item in the dictionary with the given key.
+func (d *Dictionary) Get(key string) (string, error) {
+	if d == nil {
+		return "", ErrKeyNotFound
+	}
+
+	s, err := d.abiDict.Get(key)
+	if err != nil {
+		status, ok := fastly.IsFastlyError(err)
+		switch {
+		case ok && status == fastly.FastlyStatusBadf:
+			return "", ErrDictionaryNotFound
+		case ok && status == fastly.FastlyStatusNone:
+			return "", ErrKeyNotFound
+		default:
+			return "", err
+		}
+	}
+
+	return s, nil
 }
