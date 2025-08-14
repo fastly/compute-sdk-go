456987ae655dad402a7769e27bd054ae87770f24
Damian Gryski <damian@gryski.com>
2025-08-14 11:57:07 -0700

    fsthttp: override old values in header.Apply()
    Fixes #174

--- fsthttp/headers_test.go
+++ fsthttp/headers_test.go
@@ -3,6 +3,7 @@
 package fsthttp
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -14,5 +15,21 @@ func TestHeaderBasics(t *testing.T) {
 	h.Add("Host", "zombo.com")
 	if want, have := "zombo.com", h.Get("host"); want != have {
 		t.Errorf("Host: want %q, have %q", want, have)
+	}
+}
+
+func TestHeaderApply(t *testing.T) {
+	t.Parallel()
+
+	h := NewHeader()
+	h.Add("Host", "zombo.com")
+
+	h2 := NewHeader()
+	h2.Add("Host", "zombo2.com")
+
+	h.Apply(h2)
+
+	if got, want := h.Values("Host"), []string{"zombo2.com"}; !reflect.DeepEqual(got, want) {
+		t.Errorf("Host: got %q, want %q", got, want)
 	}
 }
