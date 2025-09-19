8528bb7e9bf21ab1aaacbc6c0884e2da1fe99d77
Cameron Walters <cameron.walters@fastly.com>
2025-09-19 09:50:49 -0700

    refactor: extract doLoopbackRequest() test helper

--- end_to_end_tests/loopback/main_test.go
+++ end_to_end_tests/loopback/main_test.go
@@ -12,10 +12,39 @@ import (
 )
 
 func TestLoopback(t *testing.T) {
-	req, err := fsthttp.NewRequest("GET", "http://anyplace.horse", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("GET", func(t *testing.T) {
+		req, err := fsthttp.NewRequest("GET", "http://anyplace.horse", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp := doLoopbackRequest(t, req)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if b, want := string(body), "OK"; b != want {
+			t.Errorf("resp.Body = %s, want: %s", b, want)
+		}
+	})
+
+	t.Run("POST", func(t *testing.T) {
+		req, err := fsthttp.NewRequest("POST", "http://anyplace.horse", strings.NewReader("hello there"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp := doLoopbackRequest(t, req)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if b, want := string(body), "OK"; b != want {
+			t.Errorf("resp.Body = %s, want: %s", b, want)
+		}
+	})
+}
+
+func doLoopbackRequest(t *testing.T, req *fsthttp.Request) *fsthttp.Response {
+	t.Helper()
 	resp, err := req.Send(context.Background(), "self")
 	if err != nil {
 		t.Fatal(err)
@@ -31,38 +60,5 @@ func TestLoopback(t *testing.T) {
 	if h, want := resp.Header.Get("x-test-header"), "present"; h != want {
 		t.Errorf("X-Test-Header = %s, want: %s", h, want)
 	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if b, want := string(body), "OK"; b != want {
-		t.Errorf("resp.Body = %s, want: %s", b, want)
-	}
-
-	req, err = fsthttp.NewRequest("POST", "http://anyplace.horse", strings.NewReader("hello there"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err = req.Send(context.Background(), "self")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if h, want := resp.Header.Get("content-type"), "text/plain"; h != want {
-		t.Errorf("Content-Type = %s, want: %s", h, want)
-	}
-	if resp.Header.Get("date") == "" {
-		t.Errorf("expected default Date header is missing")
-	}
-	if h, want := resp.Header.Get("x-test-header"), "present"; h != want {
-		t.Errorf("X-Test-Header = %s, want: %s", h, want)
-	}
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if b, want := string(body), "OK"; b != want {
-		t.Errorf("resp.Body = %s, want: %s", b, want)
-	}
+	return resp
 }
