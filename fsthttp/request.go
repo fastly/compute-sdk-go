50e2c1f0bb1e17d76ede73c6ad18cbf410e1dc41
Serra <serra.abak@fastly.com>
2026-07-27 12:37:28 -0400

    Adds new ErrFanoutNotEnabled error handling

--- fsthttp/request.go
+++ fsthttp/request.go
@@ -1197,6 +1197,8 @@ type DecompressResponseOptions struct {
 
 var ErrHandoffNotSupported = errors.New("handoff not supported on this request")
 
+var ErrFanoutNotEnabled = errors.New("Fanout or WebSockets are not enabled on this service; please contact support for help")
+
 // HandoffWebsocket passes the WebSocket directly to a backend.
 //
 // This can only be used on services that have the WebSockets feature
@@ -1209,7 +1211,12 @@ func (r *Request) HandoffWebsocket(backend string) error {
 	if r.downstream.req == nil {
 		return ErrHandoffNotSupported
 	}
-	return r.downstream.req.HandoffWebsocket(backend)
+	err := r.downstream.req.HandoffWebsocket(backend)
+	status, ok := fastly.IsFastlyError(err)
+	if ok && status == fastly.FastlyStatusUnsupported {
+		return ErrFanoutNotEnabled
+	}
+	return err
 }
 
 // HandoffFanout passes the request through the Fanout GRIP proxy and on to
@@ -1224,7 +1231,12 @@ func (r *Request) HandoffFanout(backend string) error {
 	if r.downstream.req == nil {
 		return ErrHandoffNotSupported
 	}
-	return r.downstream.req.HandoffFanout(backend)
+	err := r.downstream.req.HandoffFanout(backend)
+	status, ok := fastly.IsFastlyError(err)
+	if ok && status == fastly.FastlyStatusUnsupported {
+		return ErrFanoutNotEnabled
+	}
+	return err
 }
 
 type InspectOptions struct {
